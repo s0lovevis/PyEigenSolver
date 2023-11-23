00efe3ae487f82141bc5cef8767613e277dbbd80
s0lovevis <solovevis171@gmail.com>
2023-11-23 16:48:56 +0300

    Добавил степенной метод

---
+++ eigen_solver.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def power_iteration(matrix: np.ndarray, plot: bool = False, num_iterations: int = 1000, epsilon: float = 1e-8) -> tuple[float, np.ndarray]:
+    vector = np.random.rand(matrix.shape[0])
+    vector = vector / np.linalg.norm(vector)
+
+    convergence_history = []
+
+    for i in range(num_iterations):
+        new_vector = matrix @ vector
+        new_vector = new_vector / np.linalg.norm(new_vector)
+
+        convergence = np.linalg.norm(new_vector - vector)
+        convergence_history.append(convergence)
+
+        if convergence < epsilon:
+            break
+
+        vector = new_vector
+
+    eigenvalue = vector @ matrix @ vector.T
+
+    plt.figure(figsize=(8, 6))
+    plt.plot(range(len(convergence_history)), convergence_history, marker='o', linestyle='-')
+    plt.xlabel('Итерация')
+    plt.ylabel('Сходимость')
+    plt.title('Зависимость сходимости от итерации')
+    plt.grid(True)
+
+    plt.savefig('convergence_plot.png')
+    plt.close()
+
+    return eigenvalue, vector
