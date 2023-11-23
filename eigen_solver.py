d1f13750db77087bdf5888ee97d6a7c9b488ba52
s0lovevis <solovevis171@gmail.com>
2023-11-23 22:37:28 +0300

    Чуть поменял степенной метод

--- eigen_solver.py
+++ eigen_solver.py
@@ -1,7 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def power_iteration(matrix: np.ndarray, plot: bool = False, num_iterations: int = 1000, epsilon: float = 1e-8) -> tuple[float, np.ndarray]:
+def power_iteration_method(matrix: np.ndarray, plot: bool = False, num_iterations: int = 1000, epsilon: float = 1e-8) -> tuple[float, np.ndarray]:
+
+    if matrix.shape[0] != matrix.shape[1]:
+        raise("Incorrect shape of matrix")
+
     vector = np.random.rand(matrix.shape[0])
     vector = vector / np.linalg.norm(vector)
 
@@ -19,16 +23,17 @@ def power_iteration(matrix: np.ndarray, plot: bool = False, num_iterations: int 
 
         vector = new_vector
 
-    eigenvalue = vector @ matrix @ vector.T
+    eigenvalue = np.linalg.norm(matrix @ vector) / np.linalg.norm(vector)
 
-    plt.figure(figsize=(8, 6))
-    plt.plot(range(len(convergence_history)), convergence_history, marker='o', linestyle='-')
-    plt.xlabel('Итерация')
-    plt.ylabel('Сходимость')
-    plt.title('Зависимость сходимости от итерации')
-    plt.grid(True)
+    if plot:
+        plt.figure(figsize=(8, 6))
+        plt.plot(range(len(convergence_history)), convergence_history, marker='o', linestyle='-')
+        plt.xlabel('Итерация')
+        plt.ylabel('Сходимость')
+        plt.title('Зависимость сходимости от итерации')
+        plt.grid(True)
 
-    plt.savefig('convergence_plot.png')
-    plt.close()
+        plt.savefig('convergence_plot.png')
+        plt.close()
 
     return eigenvalue, vector
