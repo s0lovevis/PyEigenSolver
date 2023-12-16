420b9ec8e0cd0b3619cc1e116c23521bc26a687e
s0lovevis <solovevis171@gmail.com>
2023-12-16 14:16:01 +0300

    добавил построение графиков во все методы

--- eigen_solver.py
+++ eigen_solver.py
@@ -34,12 +34,14 @@ def qr_decomposition(matrix: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
 
 def qr_method(matrix: np.ndarray,
               num_iterations: int = 1000,
+              plot: bool = False,
               epsilon: float = 1e-8) -> np.ndarray:
     """
     Метод QR-итераций для поиска собственных значений матрицы.
 
     Аргументы:
     matrix: np.ndarray - входная матрица
+    plot: bool - флаг для построения графика сходимости (по умолчанию False)
     num_iterations: int - максимальное количество итераций (по умолчанию 1000)
     epsilon: float - порог сходимости (по умолчанию 1e-8)
 
@@ -47,14 +49,28 @@ def qr_method(matrix: np.ndarray,
     np.ndarray: собственные значения матрицы
     """
 
+    convergence_history = []
     A_k = copy(matrix).astype(float)
     for i in range(num_iterations):
         Q, R = qr_decomposition(A_k)
         A_k = R @ Q
 
-        convergence = np.sum(np.abs(A_k - np.diag(np.diagonal(A_k))))
+        convergence = np.abs(np.min(np.diag(A_k, k=-1)))
+        convergence_history.append(convergence)
+
         if convergence < epsilon:
             break
+
+    if plot:
+        plt.figure(figsize=(8, 6))
+        plt.plot(range(len(convergence_history)), convergence_history, marker='o', linestyle='-', linewidth=3)
+        plt.xlabel('Итерация')
+        plt.ylabel('Модуль наибольшего поддиагонального элемента')
+        plt.title('Зависимость модуля наибольшего поддиагонального элемента от номера итерации')
+        plt.grid(True)
+
+        plt.savefig('convergence_plot.png')
+        plt.close()
 
     return np.diagonal(A_k)
 
@@ -100,7 +116,7 @@ def power_iteration_method(matrix: np.ndarray,
 
     if plot:
         plt.figure(figsize=(8, 6))
-        plt.plot(range(len(convergence_history)), convergence_history, marker='o', linestyle='-')
+        plt.plot(range(len(convergence_history)), convergence_history, marker='o', linestyle='-', linewidth=3)
         plt.xlabel('Итерация')
         plt.ylabel('Сходимость')
         plt.title('Зависимость сходимости от итерации')
@@ -114,6 +130,7 @@ def power_iteration_method(matrix: np.ndarray,
 
 def jacobi_rotations_method(matrix: np.ndarray,
                             num_iterations: int = 1000,
+                            plot: bool = False,
                             epsilon: float = 1e-8) -> tuple[np.ndarray, np.ndarray]:
     """
     Метод вращений Якоби для поиска всех собственных значений и собственных векторов матрицы.
@@ -121,6 +138,7 @@ def jacobi_rotations_method(matrix: np.ndarray,
     Аргументы:
     matrix: np.ndarray - входная матрица
     num_iterations: int - максимальное количество итераций (по умолчанию 1000)
+    plot: bool - флаг для построения графика сходимости (по умолчанию False)
     epsilon: float - порог сходимости (по умолчанию 1e-8)
 
     Возвращает:
@@ -135,6 +153,7 @@ def jacobi_rotations_method(matrix: np.ndarray,
                 raise ValueError("Matrix is not symmetric")
 
     cumulative_rot = np.identity(n)
+    convergence_history = []
 
     for _ in range(num_iterations):
 
@@ -145,6 +164,8 @@ def jacobi_rotations_method(matrix: np.ndarray,
                 if abs(matrix[i, j]) > max_off_diag:
                     max_off_diag = abs(matrix[i, j])
                     row, col = i, j
+
+        convergence_history.append(max_off_diag)
 
         if max_off_diag < epsilon:
             break
@@ -170,7 +191,87 @@ def jacobi_rotations_method(matrix: np.ndarray,
 
         matrix = rotation_matrix.T @ matrix @ rotation_matrix
 
+    if plot:
+        plt.figure(figsize=(8, 6))
+        plt.plot(range(len(convergence_history)), convergence_history, marker='o', linestyle='-', linewidth=3)
+        plt.xlabel('Итерация')
+        plt.ylabel('Модуль наибольшего внедиагонального элемента')
+        plt.title('Зависимость модуля наибольшего внедиагонального элемента от номера итерации')
+        plt.grid(True)
+
+        plt.savefig('convergence_plot.png')
+        plt.close()
+
     eigenvalues = np.diag(matrix)
     cumulative_rot = cumulative_rot @ rotation_matrix
 
     return eigenvalues, cumulative_rot
+
+
+def calc_det(matrix: np.ndarray) -> np.ndarray:
+    """
+    Рекурсивно вычисляет определитель квадратной матрицы методом разложения по первой строке.
+
+    Аргументы:
+    - matrix (np.ndarray): Исходная квадратная матрица.
+
+    Возвращает:
+    - np.ndarray: Определитель матрицы.
+    """
+
+    n = matrix.shape[0]
+    if n == 1:
+        return matrix[0][0]
+
+    result = np.array([0])
+
+    for i in range(n):
+        sign = 1 if i % 2 == 0 else -1
+        minor = np.delete(np.delete(matrix, 0, axis=0), i, axis=1)
+        result = np.polyadd(result,
+                            sign * np.polymul(matrix[0][i], calc_det(minor)))
+
+    return result
+
+
+def get_characteristic_polynomial(matrix: np.ndarray) -> np.ndarray:
+    """
+    Возращает коэффициенты характеристического полинома матрицы
+
+    Аргументы:
+    - matrix (np.ndarray): Исходная квадратная матрица.
+
+    Возвращает:
+    - np.ndarray:  Коэффициенты характеристического полинома матрицы
+    """
+
+    if matrix.shape[0] != matrix.shape[1]:
+        raise ValueError("Матрица должна быть квадратной")
+
+    n = matrix.shape[0]
+    new_matrix = np.zeros((n, n, 2))
+
+    for i in range(n):
+        for j in range(n):
+            if i == j:
+                new_matrix[i][j] = np.array([-1, matrix[i][j]])
+            else:
+                new_matrix[i][j] = np.array([0, matrix[i][j]])
+
+    return calc_det(new_matrix)
+
+def direct_method(matrix: np.ndarray) -> np.ndarray:
+    """
+    Поиск собственных чисел, как корней характеристического полинома
+
+    Аргументы:
+    - matrix (np.ndarray): Исходная квадратная матрица.
+
+    Возвращает:
+    - np.ndarray: Массив собственных чисел.
+    """
+
+    poly = get_characteristic_polynomial(matrix)
+
+    print(poly)
+    return np.roots(poly)
